615d2b38c4c8bd9239f9ae3a7465d1102105c660
Diego Valencia <a01784568@tec.mx>
2024-11-25 13:40:16 -0600

    Added car logic to check for cars forward, initialization of agents on corners, check for traffic lights and movements

--- server/traffic/server.py
+++ server/traffic/server.py
@@ -1,6 +1,6 @@
 from agent import *
 from model import CityModel
-from mesa.visualization import CanvasGrid
+from mesa.visualization import CanvasGrid, BarChartModule
 from mesa.visualization import ModularServer
 
 def agent_portrayal(agent):
@@ -13,21 +13,25 @@ def agent_portrayal(agent):
                  "h": 1
                  }
 
-    if (isinstance(agent, RoadAgent)):
+    if (isinstance(agent, Car)):
+        portrayal["Color"] = "black"
+        portrayal["Layer"] = 0
+
+    if (isinstance(agent, Road)):
         portrayal["Color"] = "grey"
         portrayal["Layer"] = 0
     
-    if (isinstance(agent, DestinationAgent)):
+    if (isinstance(agent, Destination)):
         portrayal["Color"] = "lightgreen"
         portrayal["Layer"] = 0
 
-    if (isinstance(agent, TrafficLightAgent)):
+    if (isinstance(agent, Traffic_Light)):
         portrayal["Color"] = "red" if not agent.state else "green"
         portrayal["Layer"] = 0
         portrayal["w"] = 0.8
         portrayal["h"] = 0.8
 
-    if (isinstance(agent, ObstacleAgent)):
+    if (isinstance(agent, Obstacle)):
         portrayal["Color"] = "cadetblue"
         portrayal["Layer"] = 0
         portrayal["w"] = 0.8
@@ -38,17 +42,17 @@ def agent_portrayal(agent):
 width = 0
 height = 0
 
-with open('./cityMap.txt') as baseFile:
+with open('../cityMap.txt') as baseFile:
     lines = baseFile.readlines()
     width = len(lines[0])-1
     height = len(lines)
 
-model_params = {"N":5}
+model_params = {"N":1}
 
 print(width, height)
 grid = CanvasGrid(agent_portrayal, width, height, 500, 500)
 
-server = ModularServer(CityModel, [grid], "Smart city", model_params)
+server = ModularServer(CityModel, [grid], "Traffic Base", model_params)
                        
 server.port = 8522 # The default
 server.launch()
